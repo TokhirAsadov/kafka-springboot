6aa6a4ccdf9b7cf7ee1bd03fce475f78cb719193
Takhir <guvalakat1603@gmail.com>
2024-08-01 10:19:25 +0500

    creating API for to send message specific partition

--- kafka-producer-example/src/main/java/uz/tokhir/controller/EventController.java
+++ kafka-producer-example/src/main/java/uz/tokhir/controller/EventController.java
@@ -17,7 +17,7 @@ public class EventController {
     @GetMapping("/publish/{message}")
     public ResponseEntity<?> publishMessage(@PathVariable String message) {
         try {
-            for (int i=0; i<10000;i++) publisher.sendMessageToTopic(message+" : "+i);
+            for (int i=0; i<100;i++) publisher.sendMessageToTopic(message+" : "+i);
             return ResponseEntity.ok("message published successfully..");
         }
         catch (Exception e) {
