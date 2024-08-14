d00de72c88b62fb2a1a1479f609020b847ff3527
Takhir <guvalakat1603@gmail.com>
2024-08-14 20:28:45 +0500

    testing

--- kafka-consumer-example/src/test/java/uz/tokhir/kafkaconsumerexample/KafkaConsumerExampleApplicationTests.java
+++ kafka-consumer-example/src/test/java/uz/tokhir/kafkaconsumerexample/KafkaConsumerExampleApplicationTests.java
@@ -1,13 +1,48 @@
 package uz.tokhir.kafkaconsumerexample;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import uz.tokhir.dto.Customer;
 
-@SpringBootTest
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@Slf4j
 class KafkaConsumerExampleApplicationTests {
 
+    @Container
+    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+
+    @DynamicPropertySource
+    public static void initKafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
+
+    @Autowired
+    private KafkaTemplate<String,Object> kafkaTemplate;
+
     @Test
-    void contextLoads() {
+    void testConsumeEvent() {
+        log.info("testConsumeEvent method execution started...");
+        kafkaTemplate.send("kafka-test",new Customer(404,"Test_Customer","test@email.com","test-number"));
+        log.info("testConsumeEvent method execution ended...");
+        await().pollInterval(Duration.ofSeconds(3))
+                .atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
+                    // assert statement
+        });
     }
 
 }
